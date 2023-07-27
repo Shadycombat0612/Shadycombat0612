f3c76dd1b9188bf319d97f155970969c712598bf
HANUMANT SINGH NEGI <140183628+Shadycombat0612@users.noreply.github.com>
2023-07-27 19:49:18 +0530

    Update max sal.js

--- max sal.js
+++ max sal.js
@@ -1,3 +1,3 @@
 var salaries=[50000, 71000, 31000, 16511, 49500, 12000, 61424]
-const maxsalary=salaries.reduce((sum,nextvalue) => (sum>nextvalue ? sum:nextvalue))
+const maxsalary=salaries.reduce((prev,next) => (prev>next ? prev:))
 console.log(maxsalary)
